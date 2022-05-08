32948c6b095086fe72599277fc96c99138a62bb0
John <nowyoutry@outlook.com>
2022-05-09 01:41:02 +0800

    Add project files.

---
+++ VideoConcat/Model/Stream.cs
@@ -0,0 +1,165 @@
+﻿using System.Diagnostics;
+using System.Text.Json.Serialization;
+
+public class Disposition
+{
+    [JsonPropertyName("default")]
+    public int Default { get; set; }
+
+    [JsonPropertyName("dub")]
+    public int Dub { get; set; }
+
+    [JsonPropertyName("original")]
+    public int Original { get; set; }
+
+    [JsonPropertyName("comment")]
+    public int Comment { get; set; }
+
+    [JsonPropertyName("lyrics")]
+    public int Lyrics { get; set; }
+
+    [JsonPropertyName("karaoke")]
+    public int Karaoke { get; set; }
+
+    [JsonPropertyName("forced")]
+    public int Forced { get; set; }
+
+    [JsonPropertyName("hearing_impaired")]
+    public int HearingImpaired { get; set; }
+
+    [JsonPropertyName("visual_impaired")]
+    public int VisualImpaired { get; set; }
+
+    [JsonPropertyName("clean_effects")]
+    public int CleanEffects { get; set; }
+
+    [JsonPropertyName("attached_pic")]
+    public int AttachedPic { get; set; }
+
+    [JsonPropertyName("timed_thumbnails")]
+    public int TimedThumbnails { get; set; }
+}
+
+public class Tags
+{
+    [JsonPropertyName("creation_time")]
+    public DateTime CreationTime { get; set; }
+
+    [JsonPropertyName("language")]
+    public string Language { get; set; }
+
+    [JsonPropertyName("encoder")]
+    public string Encoder { get; set; }
+}
+
+[DebuggerDisplay("{Width} x {Height}")]
+public class Stream
+{
+    public string Res => $"{Width} x {Height}";
+
+    [JsonPropertyName("index")]
+    public int Index { get; set; }
+
+    [JsonPropertyName("codec_name")]
+    public string CodecName { get; set; }
+
+    [JsonPropertyName("codec_long_name")]
+    public string CodecLongName { get; set; }
+
+    [JsonPropertyName("profile")]
+    public string Profile { get; set; }
+
+    [JsonPropertyName("codec_type")]
+    public string CodecType { get; set; }
+
+    [JsonPropertyName("codec_time_base")]
+    public string CodecTimeBase { get; set; }
+
+    [JsonPropertyName("codec_tag_string")]
+    public string CodecTagString { get; set; }
+
+    [JsonPropertyName("codec_tag")]
+    public string CodecTag { get; set; }
+
+    [JsonPropertyName("width")]
+    public int Width { get; set; }
+
+    [JsonPropertyName("height")]
+    public int Height { get; set; }
+
+    [JsonPropertyName("coded_width")]
+    public int CodedWidth { get; set; }
+
+    [JsonPropertyName("coded_height")]
+    public int CodedHeight { get; set; }
+
+    [JsonPropertyName("has_b_frames")]
+    public int HasBFrames { get; set; }
+
+    [JsonPropertyName("sample_aspect_ratio")]
+    public string SampleAspectRatio { get; set; }
+
+    [JsonPropertyName("display_aspect_ratio")]
+    public string DisplayAspectRatio { get; set; }
+
+    [JsonPropertyName("pix_fmt")]
+    public string PixFmt { get; set; }
+
+    [JsonPropertyName("level")]
+    public int Level { get; set; }
+
+    [JsonPropertyName("chroma_location")]
+    public string ChromaLocation { get; set; }
+
+    [JsonPropertyName("refs")]
+    public int Refs { get; set; }
+
+    [JsonPropertyName("is_avc")]
+    public string IsAvc { get; set; }
+
+    [JsonPropertyName("nal_length_size")]
+    public string NalLengthSize { get; set; }
+
+    [JsonPropertyName("r_frame_rate")]
+    public string RFrameRate { get; set; }
+
+    [JsonPropertyName("avg_frame_rate")]
+    public string AvgFrameRate { get; set; }
+
+    [JsonPropertyName("time_base")]
+    public string TimeBase { get; set; }
+
+    [JsonPropertyName("start_pts")]
+    public int StartPts { get; set; }
+
+    [JsonPropertyName("start_time")]
+    public string StartTime { get; set; }
+
+    [JsonPropertyName("duration_ts")]
+    public int DurationTs { get; set; }
+
+    [JsonPropertyName("duration")]
+    public string Duration { get; set; }
+
+    [JsonPropertyName("bit_rate")]
+    public string BitRate { get; set; }
+
+    [JsonPropertyName("bits_per_raw_sample")]
+    public string BitsPerRawSample { get; set; }
+
+    [JsonPropertyName("nb_frames")]
+    public string NbFrames { get; set; }
+
+    [JsonPropertyName("disposition")]
+    public Disposition Disposition { get; set; }
+
+    [JsonPropertyName("tags")]
+    public Tags Tags { get; set; }
+}
+
+public class Root
+{
+    [JsonPropertyName("streams")]
+    public List<Stream> Streams { get; set; }
+}
+
---
+++ VideoConcat/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+using X.Common.Helper;
+
+DirectoryInfo directory = new(@"path");
+
+List<Stream> GetStreams(string fileName)
+{
+    string raw = CommandHelper.ExecuteCommand(directory.FullName, $"ffprobe -i  \"{fileName}\" -select_streams v  -show_streams  -print_format json");
+    Regex jsonRegex = new(@"{[\w\W]+}");
+
+    string json = jsonRegex.Match(raw).Value;
+    using JsonDocument doc = JsonDocument.Parse(json);
+    var streams = doc.RootElement.GetProperty("streams").Deserialize<List<Stream>>();
+    return streams;
+}
+var files = directory.GetFiles().Where(x => x.Extension.ToLower() == ".mp4");
+int count = files.Count();
+var fileStreamInfos = files.Select(x => GetStreams(x.FullName)).ToList();
+var kinds = fileStreamInfos.SelectMany(x => x.ToList()).GroupBy(x => x.Res).Select(x => x.Key).OrderByDescending(x => x.Split('x').Select(i => Convert.ToInt32(i)).Sum()).ToList();
+
+var seq = fileStreamInfos.Select(x => x.Select(y => y.Res).ToList()).Select(x => x.IndexOf(kinds[0])).ToList();
+
+string fileList = string.Join(" -i ", files.Select(x => x.Name));
+StringBuilder sb = new($"ffmpeg -i {fileList} -vcodec h264_nvenc  -filter_complex \"");
+
+for (int i = 0; i < count; i++)
+{
+    sb.Append($"[{i}:v:{seq[i]}][{i}:a:0]");
+}
+sb.Append($"concat=n={count}:v=1:a=1[outv][outa]\" -map \"[outv]\" -map \"[outa]\" output.mp4");
+string command = sb.ToString();
+Console.WriteLine("Hello, World!");
